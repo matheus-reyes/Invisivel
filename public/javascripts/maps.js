c75c83cd4c7f49f703beb82ec1e128c04b01366e
matheus-reyes <matheuscreyes@gmail.com>
2021-08-21 00:38:15 -0300

    implementing map

---
+++ public/javascripts/maps.js
@@ -0,0 +1,20 @@
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(-23.550172, -46.624208),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    var ponto = new google.maps.LatLng(-23.550172,-46.624208);
+    var marker = new google.maps.Marker({
+        position: ponto,
+        map: map,
+        icon: "images/ping.png"
+    });
+    
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            // LÓGICA PARA ABRIR O MODAL
+        }
+    })(marker))
+}
